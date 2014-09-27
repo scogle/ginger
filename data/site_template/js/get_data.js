e14037a3219ef5cc951c47baa5abc651332bcadb
Scott Ogle <scott.ogle@semanticarts.com>
2014-09-27 16:00:44 -0600

    better default site

--- data/site_template/js/get_data.js
+++ data/site_template/js/get_data.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 	function fetch(view){
 		console.log("fetching", view);
-		$.getJSON(view.data_source, function(data){
-			console.log(view.title, data);
-		    var source = $('#'+view.id+'-template').html();
-		    var template = Handlebars.compile(source);
-		    var html = template({data:data});
-		    $('#'+view.id+'-template').after(html);
-		});
+		if ( view['data_source'] !== undefined ) {
+			$.getJSON(view.data_source, function(data){
+				console.log(view.title, data);
+			    var source = $('#'+view.id+'-template').html();
+			    var template = Handlebars.compile(source);
+			    var html = template({data:data});
+			    $('#'+view.id+'-template').replaceWith(html);
+			});
+		} else {
+			var source = $('#'+view.id+'-template').html();
+		    $('#'+view.id+'-template').replaceWith(source);
+		}
+		
 	}
 	$.each(site_json.views, function(index, view){ fetch(view); });
 });
